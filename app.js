104bf8f167a264ba8a00258f402a764e64f25b10
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-03-08 10:39:25 +0530

    home page design

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var global_variable_for_the_form_text;
+
+var app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(__dirname + '/assests'));
+app.use(express.static(__dirname + '/public'));
+
+app.get("/",function(req,res){
+	res.render("home.ejs");
+})
+
+
+
+app.listen( process.env.PORT || 8000 , function(){
+	console.log("SERVER 8000 HAS STARTED");
+});
